3dfc19fdb9aa27995a1cfd8890eb3b4d23c85db5
91928 <tejasshinde2113@gmail.com>
2024-09-16 12:14:14 -0400

    a1 commit

--- src/Kanbas/Dashboard/index.tsx
+++ src/Kanbas/Dashboard/index.tsx
@@ -37,7 +37,7 @@ export default function Dashboard() {
                     Course 3
                     </Link>
                     <p className="wd-dashboard-course-title">
-                    Full Stack software developer
+                    Full Stack software developers
                     </p>
                 </div>
             </div>
